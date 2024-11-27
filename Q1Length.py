a17b0af35f511882ab5e020800c301a7e7d907b4
Sarah Rizvi <sarah.rizvi@stu.ocsb.ca>
2024-11-27 09:41:24 -0500

    Update Q1Length.py

--- Q1Length.py
+++ Q1Length.py
@@ -1,6 +1,6 @@
 # Create a program that will accept a word from a user and return the length of that word. 
 # Make this program in a loop with option to exit when the use types in quit.
-word = input("Enter a word: ")
+word = input("Enter a word to find out it's length!: ")
 
 # Calculate the length of the word
 length = len(word)
